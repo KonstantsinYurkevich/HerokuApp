f4a8e9d972ec69ded1165a8eea7023404d24f418
GhostRider <kutcevolov@icloud.com>
2021-04-28 18:53:32 +0300

    Reformat code: 1. InputsTest 2. TypoTest Removed maven dependency for spellchecking Removed in TypoTest language library and added another way of spellchecking

--- src/test/java/TypoTest.java
+++ src/test/java/TypoTest.java
@@ -1,29 +1,14 @@
-import org.languagetool.JLanguageTool;
-import org.languagetool.language.BritishEnglish;
-import org.languagetool.rules.RuleMatch;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.io.IOException;
-import java.util.List;
 
-
-public class TypoTest extends BaseTest{
+public class TypoTest extends BaseTest {
     @Test
-    public void typoSpellcheckingTest() throws IOException {
+    public void typoSpellcheckingTest() {
         driver.get("http://the-internet.herokuapp.com/typos");
-        JLanguageTool langTool =  new JLanguageTool(new BritishEnglish());
         String textFromPage = driver.findElement(By.xpath("(((//div[2])//div[1])//div[1])//p[2]")).getText();
-        List<RuleMatch> matches = langTool.check(textFromPage);
-        for (RuleMatch match : matches) {
-            match.getFromPos();
-            match.getToPos();
-            match.getMessage();
-            match.getSuggestedReplacements();
-            String test = String.valueOf(match.getSuggestedReplacements());
-            Assert.assertEquals(test, "", "There is mistake in text");
-        }
-
+        Assert.assertEquals(textFromPage, "Sometimes you'll see a typo, other times you won't.",
+                "In text mistake");
     }
 }
